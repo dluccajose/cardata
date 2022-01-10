061282c91d7208947d86a2357ebc42219c1e39ac
Jose Dlucca <jose.dlucca@twgroup.cl>
2022-01-10 11:44:39 -0400

    show succes upload message

--- resources/views/admin/bulk-upload/index.blade.php
+++ resources/views/admin/bulk-upload/index.blade.php
@@ -5,6 +5,13 @@
         <h3>Cargar planilla de autos</h3>
     </div>
 
+    @if (session()->has('bulkupload.success'))
+        @php $response = session()->get('bulkupload.success') @endphp
+        <div class="alert alert-success">
+            {{ $response['carsCount'] }} cargados, de los cuales {{ $response['newCarsCount'] }} han sido agregados a la base de datos
+        </div>
+    @endif
+
     <div class="card p-3">
         <form action="{{ route('bulk-upload.upload') }}" method="post" enctype="multipart/form-data">
             @csrf
